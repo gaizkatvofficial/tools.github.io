7abcf10e5ff7ddfb5106eb697b3cfd1d543dc16a
gaizkatvofficial <101855980+gaizkatvofficial@users.noreply.github.com>
2024-11-23 10:07:37 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const videoConfig = {
+    file: "https://av-ch-cdn.mncnow.id/live/eds/soccerchannel-test/sa_dash_vmx/soccerchannel-test.mpd", // Ganti dengan URL HLS/DASH Anda
+    drm: {
+        clearkey: {
+            key: "7ee9506b13480491d79b71c062ab5366", // ClearKey Anda
+            keyId: "4d38060bf41b3c29df0ec950ece6b5da" // ClearKey ID Anda
+        }
+    }
+};
